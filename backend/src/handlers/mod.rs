8fb654356494a1e0414c395c3af6e5f95b1bb2d5
Kaifu49 <160703328+Kaifu49@users.noreply.github.com>
2026-06-27 21:58:44 +0530

    Delete backend directory

--- backend/src/handlers/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod health_handler;
-pub mod statement_handler;
--- backend/src/services/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod service_checker;
-pub mod storage;
-pub mod queue;
-pub mod worker;
-pub mod transaction_service;
-pub mod entity_service;
